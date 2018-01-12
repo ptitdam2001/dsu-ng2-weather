10aed6d4b5638b87602aaf2eabc616351f8efd23
Damien Suhard <ptitdam2001@gmail.com>
2018-01-12 01:20:08 +0100

    init angular 5 bootstrap

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dsu-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'dsu';
+}
